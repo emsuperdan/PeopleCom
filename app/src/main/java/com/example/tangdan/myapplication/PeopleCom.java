51a23fa0bab8bc3ce88e8a6df76d7595f7402155
emsuperdan <3291351308@qq.com>
2019-03-21 15:03:07 +0800

    â开始修改

--- app/src/main/java/com/example/tangdan/myapplication/PeopleCom.java
+++ app/src/main/java/com/example/tangdan/myapplication/PeopleCom.java
@@ -3,7 +3,7 @@ package com.example.tangdan.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class PeopleCom extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
