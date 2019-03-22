a59e11597a741662c6fc24033ec52700f8963589
emsuperdan <3291351308@qq.com>
2019-03-22 14:38:12 +0800

    增加了大部分的UI界面，只剩下评论窗口和评分窗口未完善

--- app/src/main/java/com/example/tangdan/myapplication/ui/StoreDetailActivity.java
+++ app/src/main/java/com/example/tangdan/myapplication/ui/StoreDetailActivity.java
@@ -3,12 +3,13 @@ package com.example.tangdan.myapplication.ui;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 
+import com.example.tangdan.myapplication.R;
 import com.example.tangdan.myapplication.base.BaseActivity;
 
 public class StoreDetailActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView();
+        setContentView(R.layout.detail_store);
     }
 }
