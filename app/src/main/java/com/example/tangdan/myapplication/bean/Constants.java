00002ccb53bd446c6fd3b3ab701d814fd84c8bcd
emsuperdan <3291351308@qq.com>
2019-07-13 13:29:34 +0800

    aaaa

--- app/src/main/java/com/example/tangdan/myapplication/bean/Constants.java
+++ app/src/main/java/com/example/tangdan/myapplication/bean/Constants.java
@@ -2,7 +2,7 @@ package com.example.tangdan.myapplication.bean;
 
 public class Constants {
     public class Store {
-        public static final String THIS_IS_ALPHA_TESTs;
+        adasd
         public static final String STORE_NAME = "STORE_NAME";
         public static final String STORE_IMAGE = "STORE_IMAGE";
         public static final String STORE_IMAGE_IC_LAUNCHER = "STORE_IMAGE_IC_LAUNCHER";
