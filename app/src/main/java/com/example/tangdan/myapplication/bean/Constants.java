cf976e5e5624a4d497a9e983a6aab16e61290b76
emsuperdan <3291351308@qq.com>
2019-07-13 14:01:57 +0800

    onecommit

--- app/src/main/java/com/example/tangdan/myapplication/bean/Constants.java
+++ app/src/main/java/com/example/tangdan/myapplication/bean/Constants.java
@@ -2,7 +2,7 @@ package com.example.tangdan.myapplication.bean;
 
 public class Constants {
     public class Store {
-        adasd
+        onecommit
         public static final String STORE_NAME = "STORE_NAME";
         public static final String STORE_IMAGE = "STORE_IMAGE";
         public static final String STORE_IMAGE_IC_LAUNCHER = "STORE_IMAGE_IC_LAUNCHER";
