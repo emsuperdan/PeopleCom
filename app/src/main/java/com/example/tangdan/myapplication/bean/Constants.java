b0f5464114873a176eb336b96c21fa8505728165
emsuperdan <3291351308@qq.com>
2019-07-13 13:18:41 +0800

    branch2第ächange
    (cherry picked from commit e4e4627)

--- app/src/main/java/com/example/tangdan/myapplication/bean/Constants.java
+++ app/src/main/java/com/example/tangdan/myapplication/bean/Constants.java
@@ -2,7 +2,7 @@ package com.example.tangdan.myapplication.bean;
 
 public class Constants {
     public class Store {
-        public static final String THIS_IS_ALPHA_TESTs;
+        ALPHA_TESTs
         public static final String STORE_NAME = "STORE_NAME";
         public static final String STORE_IMAGE = "STORE_IMAGE";
         public static final String STORE_IMAGE_IC_LAUNCHER = "STORE_IMAGE_IC_LAUNCHER";
