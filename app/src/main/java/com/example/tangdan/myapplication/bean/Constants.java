de19be8ff8cb2c7d90a4350ece0096050f9b9d8a
tangdan <tangdan@xiaomi.com>
2019-07-14 11:32:52 +0800

    我开始在branch4的子分支上进行开发了，大家注意啊，看看它又没哟分叉

--- app/src/main/java/com/example/tangdan/myapplication/bean/Constants.java
+++ app/src/main/java/com/example/tangdan/myapplication/bean/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
         public static final String STORE_IMAGE = "STORE_IMAGE";
         public static final String STORE_IMAGE_IC_LAUNCHER = "STORE_IMAGE_IC_LAUNCHER";
         public static final String STORE_OBJECT_ID = "STORE_OBJECT_ID";
-        我开始在branch4上进行开发了，大家注意啊,可是这样为什么我的分之还是没有分叉呢
+        我开始在branch4的子分支上进行开发了，大家注意啊，看看它又没哟分叉
         public static final String USER_ACCOUNT = "USER_ACCOUNT";
         public static final String USER_PASSWORD = "USER_PASSWORD";
         public static final String USER_OBJECT_ID = "USER_OBJECT_ID";
