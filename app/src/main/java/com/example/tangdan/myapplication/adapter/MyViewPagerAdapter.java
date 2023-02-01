4ea0dd26209243b51765d95f5b41f9fb20b6a5d1
tangdan <fyztd123>
2023-02-01 18:27:44 +0800

    #Feature Migrate to androidx test

--- app/src/main/java/com/example/tangdan/myapplication/adapter/MyViewPagerAdapter.java
+++ app/src/main/java/com/example/tangdan/myapplication/adapter/MyViewPagerAdapter.java
@@ -6,6 +6,7 @@ import android.view.ViewGroup;
 import android.widget.ImageView;
 import androidx.viewpager.widget.ViewPager;
 import androidx.viewpager.widget.PagerAdapter;
+
 import java.util.ArrayList;
 
 public class MyViewPagerAdapter extends PagerAdapter {
